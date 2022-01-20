78e3098a3b4b1c8197fce15e86116d1b12ba87c0
geniusvale <ariiqromero@gmail.com>
2022-01-20 15:49:23 +0700

    add activity for every menu with intent

--- app/src/main/java/com/aroman/taqwa/MainActivity.java
+++ app/src/main/java/com/aroman/taqwa/MainActivity.java
@@ -2,13 +2,45 @@ package com.aroman.taqwa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.card.MaterialCardView;
 
 public class MainActivity extends AppCompatActivity {
+    MaterialCardView cardSholatWajib, cardBacaanSholat, cardDoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cardSholatWajib = findViewById(R.id.cardSholatWajib);
+        cardBacaanSholat = findViewById(R.id.cardBacaanSholat);
+        cardDoa = findViewById(R.id.cardDoa);
+
+        cardSholatWajib.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, NiatSholat.class));
+            }
+        });
+
+        cardBacaanSholat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, BacaanSholat.class));
+            }
+        });
+
+        cardDoa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Doa.class));
+            }
+        });
+
+
     }
 }
